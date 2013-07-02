83bbce05958e1c46ad720f499dd55d12861f9779
phi <phi1618jp@gmail.com>
2013-07-03 03:31:01 +0900

    support less

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.configure(function(){
   app.use(express.cookieParser());
   app.use(express.session({secret: "mikan oishii"}));
   app.use(app.router);
+  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
