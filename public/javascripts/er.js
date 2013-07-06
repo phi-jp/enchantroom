5e8977863a9817b1fa286a6ba01350802f9fa277
phi <phi1618jp@gmail.com>
2013-07-07 04:57:46 +0900

    bug fix

--- public/javascripts/er.js
+++ public/javascripts/er.js
@@ -5,7 +5,7 @@
 
 ;(function(global) {
 
-	var socket = io.connect('http://'+location.host+':'+80);
+	var socket = io.connect('http://'+location.host+':'+3000);
 
 	socket.on('announce', function(data) {
 		var data = JSON.parse(data);
